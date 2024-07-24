60fffbe4baee717cced400449dc502c6fc904afd
AliAlhadi <alialhadiabokhalil@gmail.com>
2024-07-24 16:00:58 +0300

    first commit

---
+++ WorldDumbestFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting
+{
+    internal class WorldDumbestFunction
+    {
+        public string ReturnPickachu(int num)
+        {
+            if (num == 0)
+            {
+                return "pickachu";
+            }
+            else
+            {
+                return "Squirtle";
+            }
+        }
+    }
+}
